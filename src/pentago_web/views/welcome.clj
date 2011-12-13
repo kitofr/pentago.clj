d2441c4eb4b5d416301b67e1cc5db676954b1677
Kristoffer Roupé <kitofr@gmail.com>
2011-12-13 22:52:07 +0100

    One player at a time

--- src/pentago_web/views/welcome.clj
+++ src/pentago_web/views/welcome.clj
@@ -6,8 +6,7 @@
             [noir.session :as session]
             [noir.content.getting-started]
             [clj-json [core :as json]])
-  (:use [noir.core :only [defpage defpartial]]
-        [hiccup.core :only [html]]))
+  (:use [noir.core :only [defpage]]))
 
 (defn json-data [rq]
   (:json-data (:params rq)))
@@ -20,11 +19,11 @@
 
 (defn board [id] ((session/get id) :board))
 (defn players [id] ((session/get id) :players))
-(defn next-player [id] (first (next (cycle (players id)))))
+(defn in-turn [id] (first (cycle (players id))))
 
 (defn game-state [id]
   { :game {:board (board id) 
-           :next  (next-player id)
+           :in-turn  (in-turn id)
            :players (players id) } } )
 
 (defn swap-players [id]
@@ -42,29 +41,42 @@
 (defpage "/game/:id" {game-id :id}
          (resp/ok (game-state game-id)))
 
+(defn is-playing? [player id]
+  (= player (in-turn id)))
+
 (defpage [:put "/game/:id/set"] { game-id :id }
          (let [rq (json-data (req/ring-request))
                space (:space rq)
                player (:player rq)
                board (board game-id)]
-           (if (game/available? space board)
+           (cond 
+             (not (is-playing? player game-id)) (resp/forbidden (game-state game-id))
+             (not (game/available? space board)) (resp/conflict (game-state game-id))
+             true 
              (do 
                (store! game-id (game/move player space board) (players game-id)) 
                (resp/accepted (merge { :links { :self (str "game/" game-id) 
                                                :turn (str "game/" game-id "/turn/{corner}")
                                                :pass (str "game/" game-id "/pass") } } 
-                                     (game-state game-id ))))
-             (resp/conflict (game-state game-id)))))
+                                     (game-state game-id )))))))
 
 (defpage [:post "/game/:id/pass"] { game-id :id }
-         (let [rq (json-data (req/ring-request))]
-           (store! game-id (board game-id) (swap-players game-id))
-           (resp/accepted (game-state game-id))))
+         (let [rq (json-data (req/ring-request))
+               player (:player rq)]
+           (if (not (is-playing? player game-id)) 
+             (resp/forbidden (game-state game-id))
+             (do
+               (store! game-id (board game-id) (swap-players game-id))
+               (resp/accepted (game-state game-id))))))
 
 (defpage [:put "/game/:id/turn"] { game-id :id }
          (let [board (board game-id)
                rq (json-data (req/ring-request))
+               player (:player rq)
                corner (:corner rq)
                dir (:dir rq)]
-           (store! game-id (game/restore-board (game/turn corner dir board)) (swap-players game-id))
-           (resp/accepted (game-state game-id))))
+           (if (not (is-playing? player game-id))
+             (resp/forbidden (game-state game-id))
+             (do
+               (store! game-id (game/restore-board (game/turn corner dir board)) (swap-players game-id))
+               (resp/accepted (game-state game-id))))))
